9dcc48bb7f1c849ae5ff0822a72f82191693370b
usman_Dev <akeemusman32@gmail.com>
2025-08-02 15:58:16 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const shareIcon =
+  document.getElementById('icon');
+const sharePopup =
+  document.getElementById(
+    'share-popup'
+  );
+
+document.addEventListener(
+  'click',
+  () => {
+    if (
+      sharePopup.classList.contains(
+        'hidden'
+      )
+    ) {
+      sharePopup.classList.remove(
+        'hidden'
+      );
+    } else {
+      sharePopup.classList.add(
+        'hidden'
+      );
+    }
+  }
+);
